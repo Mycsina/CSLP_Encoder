4b96741e4fd9aa4638cbba04ee9da5a2c3b40597
BrunoPascoa76 <brunopascoa03@gmail.com>
2023-12-22 15:51:10 +0000

    core part of the dct done Now for the rle

---
+++ src/codec/encoders/DCTEncoder.cpp
@@ -0,0 +1,45 @@
+#include "DCTEncoder.hpp";
+
+using namespace std;
+using namespace cv;
+
+#ifndef PI
+#define PI 3.14159265358979324
+#endif
+
+DCTEncoder::~DCTEncoder(){
+    delete[] y_qmat;
+    delete[] uv_qmat;
+    delete[] zigzag_order;
+}
+
+void DCTEncoder::dct8x8(int (&in)[8][8], double (&out)[8][8]) {
+    double sum;
+    for(int I=0; I<8; I++){
+        for(int II=0; II<8; II++){
+            sum=0;
+            for (int r=0; r<8; r++){
+                for (int c=0; c<8; c++){
+                    sum+=in[r][c] * cos((2*r+1)*I*PI/16) * cos((2*c+1)*II*PI/16) * ((I==0)?1/sqrt(2):1) * ((II==0)?1/sqrt(2):1); //I got this formula from the internet as Fourier transform is complicated
+                }
+            }
+            out[I][II]=sum/4;
+        }
+    }
+}
+
+void DCTEncoder::idct8x8(double (&in)[8][8], int (&out)[8][8]) {
+    double sum;
+
+    for(int I=0; I<8; I++){
+        for(int II=0; II<8; II++){
+            sum=0;
+            for (int r=0; r<8; r++){
+                for (int c=0; c<8; c++){
+                    sum+=in[r][c] * cos((2*I+1)*c*PI/16) * cos((2*II+1)*v*PI/16) * ((r==0)?1/sqrt(2):1) * ((c==0)?1/sqrt(2):1); //I got this formula from the internet as Fourier transform is complicated
+                }
+            }
+            out[I][II]=(int)sum/4;
+        }
+    }
+}
